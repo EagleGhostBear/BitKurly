bd69588aeff7823ac4b189393320b8b1af7ed298
EagleGhostBear <eagle11062@gmail.com>
2026-02-06 11:43:50 +0900

    feat(product): 상품 정보 프로젝트 추가 - product-service 추가 - build.gradle 버전 호환성 변경

---
+++ apps/backend/product-service/src/test/java/com/bitkulry/product/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitkulry.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
